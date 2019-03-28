010b1df9c38fcaa44951da800022f589e131ef92
Xianjun Jiao <putaoshu@msn.com>
2019-03-28 21:11:06 +0100

    update

--- host/btle-tools/rf_driver/rf_driver_hackrf.c
+++ host/btle-tools/rf_driver/rf_driver_hackrf.c
@@ -37,22 +37,22 @@ static int hackrf_rate_internal = -1;
 static int hackrf_bw_internal = -1;
 //---------------------for TX---------------------------
 
-volatile int hackrf_stop_tx = 1;
-volatile int hackrf_tx_len;
-volatile void *hackrf_tx_len;
+volatile int hackrf_stop_tx_internal = 1;
+volatile int hackrf_tx_len_internal;
+volatile void *hackrf_tx_buf_internal;
 
 int hackrf_tx_callback(hackrf_transfer* transfer) {
   int size_left;
-  if (hackrf_stop_tx == 0) {
+  if (hackrf_stop_tx_internal == 0) {
     memset(transfer->buffer, 0, NUM_PRE_SEND_DATA);
-    memcpy(transfer->buffer+NUM_PRE_SEND_DATA, (char *)(hackrf_tx_len), hackrf_tx_len);
+    memcpy(transfer->buffer+NUM_PRE_SEND_DATA, (char *)(hackrf_tx_buf_internal), hackrf_tx_len_internal);
 
-    size_left = (transfer->valid_length - hackrf_tx_len - NUM_PRE_SEND_DATA);
-    memset(transfer->buffer+NUM_PRE_SEND_DATA+hackrf_tx_len, 0, size_left);
+    size_left = (transfer->valid_length - hackrf_tx_len_internal - NUM_PRE_SEND_DATA);
+    memset(transfer->buffer+NUM_PRE_SEND_DATA+hackrf_tx_len_internal, 0, size_left);
   } else {
     memset(transfer->buffer, 0, transfer->valid_length);
   }
-  hackrf_stop_tx++;
+  hackrf_stop_tx_internal++;
  
   return(0);
 }
@@ -177,73 +177,87 @@ int hackrf_update_bw(void *device, int *bw) {
   return(HACKRF_SUCCESS);
 }
 
-int hackrf_init_board() {
-	int result = hackrf_init();
-	if( result != HACKRF_SUCCESS ) {
-		fprintf(stderr,"hackrf_init_board: hackrf_init() failed: %s (%d)\n", hackrf_error_name(result), result);
-		//print_usage();
-		return(EXIT_FAILURE);
-	}
+int hackrf_stop_close_board(void* device, void *trx_input){
+  struct trx_cfg_op *trx = (struct trx_cfg_op *)trx_input;
+  hackrf_device *dev = (hackrf_device* )device;
 
+  if (dev==NULL)
+    return;
+
+  if (trx->tx.en) {
+    result = hackrf_stop_tx(dev);
+    if( result != HACKRF_SUCCESS ) {
+      printf("hackrf_stop_close_board: hackrf_stop_tx() failed: %s (%d)\n", hackrf_error_name(result), result);
+      return(-1);
+    }
+  }
+  
+  if (trx->rx.en) {
+    result = hackrf_stop_rx(dev);
+    if( result != HACKRF_SUCCESS ) {
+      printf("hackrf_stop_close_board: hackrf_stop_rx() failed: %s (%d)\n", hackrf_error_name(result), result);
+      return(-1);
+    }
+  }
+
+  result = hackrf_close(dev);
+  if( result != HACKRF_SUCCESS )
+  {
+    printf("hackrf_stop_close_board: hackrf_close() failed: %s (%d)\n", hackrf_error_name(result), result);
+    return(-1);
+  }
+
+  hackrf_exit();
   return(0);
 }
 
-inline int hackrf_open_board(uint64_t freq_hz, int gain, hackrf_device** device) {
+int hackrf_tx_one_buf(void *dev, void *buffer, int *len) {
   int result;
+  hackrf_device *device = dev;
 
-	result = hackrf_open(device);
-	if( result != HACKRF_SUCCESS ) {
-		printf("hackrf_open_board: hackrf_open() failed: %s (%d)\n", hackrf_error_name(result), result);
- //   print_usage();
-		return(-1);
-	}
+  hackrf_tx_len_internal = (*len);
+  hackrf_tx_buf_internal = buffer;
+#if 0
+  // open the board-----------------------------------------
+  if (open_board() == -1) {
+    printf("tx_one_buf: open_board() failed\n");
+    return(-1);
+  }
+#endif
+  // first round TX---------------------------------
+  hackrf_stop_tx = 0;
 
-  result = hackrf_set_freq(*device, freq_hz);
+  result = hackrf_start_tx(device, hackrf_tx_callback, NULL);
   if( result != HACKRF_SUCCESS ) {
-    printf("hackrf_open_board: hackrf_set_freq() failed: %s (%d)\n", hackrf_error_name(result), result);
- //   print_usage();
+    printf("hackrf_tx_one_buf: hackrf_start_tx() failed: %s (%d)\n", hackrf_error_name(result), result);
     return(-1);
   }
 
-  result = hackrf_set_sample_rate(*device, SAMPLE_PER_SYMBOL*1000000ul);
-  if( result != HACKRF_SUCCESS ) {
-    printf("hackrf_open_board: hackrf_set_sample_rate() failed: %s (%d)\n", hackrf_error_name(result), result);
- //   print_usage();
+  while( (hackrf_is_streaming(device) == HACKRF_TRUE) && (do_exit == false) ) {
+    if (hackrf_stop_tx_internal>=9)
+      break;
+  }
+
+  if (do_exit)
+  {
+    printf("\ntx_one_buf: Exiting...\n");
     return(-1);
   }
-  
-  result = hackrf_set_baseband_filter_bandwidth(*device, SAMPLE_PER_SYMBOL*1000000ul/2);
+#if 0
+  result = hackrf_stop_tx(device);
   if( result != HACKRF_SUCCESS ) {
-    printf("hackrf_open_board: hackrf_set_baseband_filter_bandwidth() failed: %s (%d)\n", hackrf_error_name(result), result);
- //   print_usage();
-    return(-1);
-  }
-  
-  result = hackrf_set_vga_gain(*device, gain);
-	result |= hackrf_set_lna_gain(*device, HACKRF_MAX_LNA_GAIN);
-  if( result != HACKRF_SUCCESS ) {
-    printf("hackrf_open_board: hackrf_set_txvga_gain() failed: %s (%d)\n", hackrf_error_name(result), result);
-//    print_usage();
+    printf("tx_one_buf: hackrf_stop_tx() failed: %s (%d)\n", hackrf_error_name(result), result);
     return(-1);
   }
 
-  return(0);
-}
-
-inline int hackrf_run_board(hackrf_device* device) {
-  int result;
-
-	result = hackrf_stop_rx(device);
-	if( result != HACKRF_SUCCESS ) {
-		printf("run_board: hackrf_stop_rx() failed: %s (%d)\n", hackrf_error_name(result), result);
-		return(-1);
-	}
-  
-  result = hackrf_start_rx(device, hackrf_rx_callback, NULL);
-  if( result != HACKRF_SUCCESS ) {
-    printf("run_board: hackrf_start_rx() failed: %s (%d)\n", hackrf_error_name(result), result);
+  // close the board---------------------------------------
+  if (close_board() == -1) {
+    printf("tx_one_buf: close_board() failed\n");
     return(-1);
   }
+#endif
+
+  do_exit = false;
 
   return(0);
 }
@@ -359,7 +373,7 @@ int hackrf_config_run_board(struct trx_cfg_op *trx) {
     trx->tx.update_gain = hackrf_update_tx_gain;
     trx->tx.update_rate = hackrf_update_rate;
     trx->tx.update_bw = hackrf_update_bw;
-    trx->tx.proc_one_buf = ;
+    trx->tx.proc_one_buf = hackrf_tx_one_buf;
   }
 
   if (trx->rx.en) {
@@ -374,94 +388,6 @@ int hackrf_config_run_board(struct trx_cfg_op *trx) {
   }
 
   trx->stop_close = hackrf_stop_close_board;
-
-  return(0);
-}
-
-int hackrf_stop_close_board(void* device, void *trx_input){
-  struct trx_cfg_op *trx = (struct trx_cfg_op *)trx_input;
-  hackrf_device *dev = (hackrf_device* )device;
-
-  if (dev==NULL)
-    return;
-
-  if (trx->tx.en) {
-    result = hackrf_stop_tx(dev);
-    if( result != HACKRF_SUCCESS ) {
-      printf("hackrf_stop_close_board: hackrf_stop_tx() failed: %s (%d)\n", hackrf_error_name(result), result);
-      return(-1);
-    }
-  }
-  
-  if (trx->rx.en) {
-    result = hackrf_stop_rx(dev);
-    if( result != HACKRF_SUCCESS ) {
-      printf("hackrf_stop_close_board: hackrf_stop_rx() failed: %s (%d)\n", hackrf_error_name(result), result);
-      return(-1);
-    }
-  }
-
-  result = hackrf_close(dev);
-  if( result != HACKRF_SUCCESS )
-  {
-    printf("hackrf_stop_close_board: hackrf_close() failed: %s (%d)\n", hackrf_error_name(result), result);
-    return(-1);
-  }
-
-  hackrf_exit();
-  return(0);
-}
-
-//---------------------------FROM TX-------------
-
-inline int hackrf_tx_one_buf(void *dev, char *buf, int length) {
-  int result;
-
-  hackrf_tx_len = buf;
-  hackrf_tx_len = length;
-
-  // open the board-----------------------------------------
-  if (open_board() == -1) {
-    printf("tx_one_buf: open_board() failed\n");
-    return(-1);
-  }
-
-  // first round TX---------------------------------
-  hackrf_stop_tx = 0;
-
-  result = hackrf_start_tx(device, hackrf_tx_callback, NULL);
-  if( result != HACKRF_SUCCESS ) {
-    printf("tx_one_buf: hackrf_start_tx() failed: %s (%d)\n", hackrf_error_name(result), result);
-    return(-1);
-  }
-
-  while( (hackrf_is_streaming(device) == HACKRF_TRUE) &&
-      (do_exit == false) )
-  {
-    if (hackrf_stop_tx>=9) {
-      break;
-    }
-  }
-
-  if (do_exit)
-  {
-    printf("\ntx_one_buf: Exiting...\n");
-    return(-1);
-  }
-
-  result = hackrf_stop_tx(device);
-  if( result != HACKRF_SUCCESS ) {
-    printf("tx_one_buf: hackrf_stop_tx() failed: %s (%d)\n", hackrf_error_name(result), result);
-    return(-1);
-  }
-
-  // close the board---------------------------------------
-  if (close_board() == -1) {
-    printf("tx_one_buf: close_board() failed\n");
-    return(-1);
-  }
-
-  do_exit = false;
 
   return(0);
 }
