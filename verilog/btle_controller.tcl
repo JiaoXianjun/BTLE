08a31806a85e662c3091768013de6f7481f6a753
Xianjun Jiao <putaoshu@msn.com>
2024-04-08 20:05:37 +0200

    Change the xdc filename

--- verilog/btle_controller.tcl
+++ verilog/btle_controller.tcl
@@ -676,9 +676,9 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
-set file "[file normalize "$origin_dir/btle_controller.xdc"]"
+set file "[file normalize "$origin_dir/btle_controller_wrapper.xdc"]"
 set file_added [add_files -norecurse -fileset $obj [list $file]]
-set file "btle_controller.xdc"
+set file "btle_controller_wrapper.xdc"
 set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
 set_property -name "file_type" -value "XDC" -objects $file_obj
 set_property -name "is_enabled" -value "1" -objects $file_obj
@@ -696,8 +696,8 @@ set_property -name "used_in_synthesis" -value "1" -objects $file_obj
 set obj [get_filesets constrs_1]
 set_property -name "constrs_type" -value "XDC" -objects $obj
 set_property -name "name" -value "constrs_1" -objects $obj
-set_property -name "target_constrs_file" -value "[get_files *btle_controller.xdc]" -objects $obj
-set_property -name "target_ucf" -value "[get_files *btle_controller.xdc]" -objects $obj
+set_property -name "target_constrs_file" -value "[get_files *btle_controller_wrapper.xdc]" -objects $obj
+set_property -name "target_ucf" -value "[get_files *btle_controller_wrapper.xdc]" -objects $obj
 
 # Create 'sim_1' fileset (if not found)
 if {[string equal [get_filesets -quiet sim_1] ""]} {
--- verilog/btle_controller_wrapper.xdc
+++ verilog/btle_controller_wrapper.xdc
@@ -1,3 +1,7 @@
+# Author: Xianjun Jiao <putaoshu@msn.com>
+# SPDX-FileCopyrightText: 2024 Xianjun Jiao
+# SPDX-License-Identifier: Apache-2.0 license
+
 create_clock -period 62.500 -name clk -waveform {0.000 31.250} [get_ports clk]
 set_input_delay -clock [get_clocks clk] -min -add_delay 10.000 [get_ports {rx_i_signal[*]}]
 set_input_delay -clock [get_clocks clk] -max -add_delay 20.000 [get_ports {rx_i_signal[*]}]
@@ -21,3 +25,5 @@ set_output_delay -clock [get_clocks clk] -min -add_delay 5.000 [get_ports tx_iq_
 set_output_delay -clock [get_clocks clk] -max -add_delay 11.000 [get_ports tx_iq_valid_last]
 set_output_delay -clock [get_clocks clk] -min -add_delay 5.000 [get_ports uart_tx]
 set_output_delay -clock [get_clocks clk] -max -add_delay 11.000 [get_ports uart_tx]
+
+set_property BITSTREAM.General.UnconstrainedPins {Allow} [current_design]
