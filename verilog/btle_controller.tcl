c95f82179ac4ea50d0ec4bb8fa27d4715729d13b
Xianjun Jiao <putaoshu@msn.com>
2025-07-30 22:16:14 +0200

    Add tdpram to btle_controller.tcl

--- verilog/btle_controller.tcl
+++ verilog/btle_controller.tcl
@@ -189,6 +189,7 @@ set files [list \
  [file normalize "${origin_dir}/crc24.v"] \
  [file normalize "${origin_dir}/crc24_core.v"] \
  [file normalize "${origin_dir}/dpram.v"] \
+ [file normalize "${origin_dir}/tdpram.v"] \
  [file normalize "${origin_dir}/gauss_filter.v"] \
  [file normalize "${origin_dir}/gfsk_demodulation.v"] \
  [file normalize "${origin_dir}/gfsk_modulation.v"] \
@@ -334,6 +335,19 @@ set_property -name "used_in_simulation" -value "1" -objects $file_obj
 set_property -name "used_in_synthesis" -value "1" -objects $file_obj
 
 set file "$origin_dir/dpram.v"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "file_type" -value "Verilog" -objects $file_obj
+set_property -name "is_enabled" -value "1" -objects $file_obj
+set_property -name "is_global_include" -value "0" -objects $file_obj
+set_property -name "library" -value "xil_defaultlib" -objects $file_obj
+set_property -name "path_mode" -value "RelativeFirst" -objects $file_obj
+set_property -name "used_in" -value "synthesis implementation simulation" -objects $file_obj
+set_property -name "used_in_implementation" -value "1" -objects $file_obj
+set_property -name "used_in_simulation" -value "1" -objects $file_obj
+set_property -name "used_in_synthesis" -value "1" -objects $file_obj
+
+set file "$origin_dir/tdpram.v"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "Verilog" -objects $file_obj
