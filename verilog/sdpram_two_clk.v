c1d7c7c868d86e29e32e69dc968349ada0753c07
Xianjun Jiao <putaoshu@msn.com>
2025-07-31 20:22:30 +0200

    Update dpram according to Xilinx: UG901 2025-06-11 https://docs.amd.com/r/en-US/ug901-vivado-synthesis/Simple-Dual-Port-Block-RAM-with-Single-Clock-Verilog Simple Dual-Port Block RAM with Single Clock (Verilog) Simple Dual-Port Block RAM with Dual Clocks (Verilog)

---
+++ verilog/sdpram_two_clk.v
@@ -0,0 +1,41 @@
+// Author: Xianjun Jiao <putaoshu@msn.com>
+// SPDX-FileCopyrightText: 2025 Xianjun Jiao
+// SPDX-License-Identifier: Apache-2.0 license
+
+// Based on Xilinx UG901 2025-06-11
+// https://docs.amd.com/r/en-US/ug901-vivado-synthesis/Simple-Dual-Port-Block-RAM-with-Single-Clock-Verilog
+// Simple Dual-Port Block RAM with Dual Clocks (Verilog)
+
+`timescale 1ns / 1ps
+module sdpram_two_clk #
+(
+  parameter DATA_WIDTH = 8,
+  parameter ADDRESS_WIDTH = 11
+) (
+  input wire clk,
+  input wire rst,
+
+  input wire [ADDRESS_WIDTH-1:0] write_address,
+  input wire [DATA_WIDTH-1:0] write_data,
+  input wire write_enable,
+
+  input wire clkb,
+  input wire [ADDRESS_WIDTH-1:0] read_address,
+  output reg [DATA_WIDTH-1:0] read_data
+);
+
+reg [DATA_WIDTH-1:0] memory [((1<<ADDRESS_WIDTH)-1):0];
+
+// Write logic (Port A)
+always @ (posedge clk) begin
+  if (write_enable) begin
+    memory[write_address] <= write_data;
+  end
+end
+
+// Read logic (Port B)
+always @ (posedge clkb) begin
+  read_data <= memory[read_address];
+end
+
+endmodule
