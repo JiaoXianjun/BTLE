b9f4e1cfa9aa7ab85d1a05682b82ecb557bc957b
Xianjun Jiao <putaoshu@msn.com>
2025-08-20 23:03:12 +0200

    track sub modules

---
+++ fpga/sdrpi/helpers/save_fpga_img_ila.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# // Author: Xianjun Jiao <putaoshu@msn.com>
+# // SPDX-FileCopyrightText: 2025 Xianjun Jiao
+# // SPDX-License-Identifier: Apache-2.0 license
+
+cp ../btle_sdrpi/system_top.xsa ../../../BTLE-hw-img/fpga/sdrpi/system_top.xsa
+cp ../btle_sdrpi/btle_sdrpi.runs/impl_1/system_top.ltx ../../../BTLE-hw-img/fpga/sdrpi/system_top.ltx
