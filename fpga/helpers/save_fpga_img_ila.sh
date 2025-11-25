6fbf8270099894001309187864efd51429b96fa6
Xianjun Jiao <putaoshu@msn.com>
2025-11-25 21:37:35 +0100

    Change flow of save_fpga_img_ila.sh: by default it will save fpga img in the temp directory in the current directory. only when FPGA project not existing, it will fetch fpga img from the official repository into the temp directory in the cureent directory

--- fpga/helpers/save_fpga_img_ila.sh
+++ fpga/helpers/save_fpga_img_ila.sh
@@ -13,18 +13,38 @@ else
   HARDWARE=$1
 fi
 
+OFFICIAL_DIR_SAVE_FPGA_IMG=../../BTLE-hw-img/fpga/$HARDWARE
+TMP_DIR_SAVE_FPGA_IMG=./BTLE-hw-img/fpga/$HARDWARE
+
 if [ -f "../$HARDWARE/btle_$HARDWARE/system_top.xsa" ]; then
-  cp ../$HARDWARE/btle_$HARDWARE/system_top.xsa ../../BTLE-hw-img/fpga/$HARDWARE/system_top.xsa
+  mkdir -p $TMP_DIR_SAVE_FPGA_IMG
+  rm -rf $TMP_DIR_SAVE_FPGA_IMG/*
+
+  cp ../$HARDWARE/btle_$HARDWARE/system_top.xsa $TMP_DIR_SAVE_FPGA_IMG/system_top.xsa
 else
-  echo "../$HARDWARE/btle_$HARDWARE/system_top.xsa does not exist. Please check!"
-  exit 1
+  echo "../$HARDWARE/btle_$HARDWARE/system_top.xsa does not exist."
+  echo "Try to fetch FPGA img from $OFFICIAL_DIR_SAVE_FPGA_IMG and save to $TMP_DIR_SAVE_FPGA_IMG/"
+  if [ -f "$OFFICIAL_DIR_SAVE_FPGA_IMG/system_top.xsa" ]; then
+    mkdir -p $TMP_DIR_SAVE_FPGA_IMG
+    rm -rf $TMP_DIR_SAVE_FPGA_IMG/*
+
+    cp $OFFICIAL_DIR_SAVE_FPGA_IMG/system_top.xsa $TMP_DIR_SAVE_FPGA_IMG/system_top.xsa
+  else
+    echo "$OFFICIAL_DIR_SAVE_FPGA_IMG/system_top.xsa does not exist. Please check!"
+    exit 1
+  fi
 fi
 
 if [ -f "../$HARDWARE/btle_$HARDWARE/btle_$HARDWARE.runs/impl_1/system_top.ltx" ]; then
-  cp ../$HARDWARE/btle_$HARDWARE/btle_$HARDWARE.runs/impl_1/system_top.ltx ../../BTLE-hw-img/fpga/$HARDWARE/system_top.ltx
+  cp ../$HARDWARE/btle_$HARDWARE/btle_$HARDWARE.runs/impl_1/system_top.ltx $TMP_DIR_SAVE_FPGA_IMG/system_top.ltx
 else
-  echo "../$HARDWARE/btle_$HARDWARE/btle_$HARDWARE.runs/impl_1/system_top.ltx does not exist. Please check!"
-  exit 1
+  echo "../$HARDWARE/btle_$HARDWARE/btle_$HARDWARE.runs/impl_1/system_top.ltx does not exist."
+  echo "Try to fetch FPGA img from $OFFICIAL_DIR_SAVE_FPGA_IMG and save to $TMP_DIR_SAVE_FPGA_IMG/"
+  if [ -f "$OFFICIAL_DIR_SAVE_FPGA_IMG/system_top.ltx" ]; then
+    cp $OFFICIAL_DIR_SAVE_FPGA_IMG/system_top.ltx $TMP_DIR_SAVE_FPGA_IMG/system_top.ltx
+  else
+    echo "$OFFICIAL_DIR_SAVE_FPGA_IMG/system_top.ltx does not exist. Skip it."
+  fi
 fi
 
 # HARDWARES="sdrpi antsdr_e200 antsdr"
