4adfc217769d01352506f863b97fedec93e102bb
Xianjun Jiao <putaoshu@msn.com>
2026-04-12 22:49:14 +0200

    simplify firmware compiling script

--- firmware/compile.sh
+++ firmware/compile.sh
@@ -11,17 +11,23 @@ source ~/Xilinx/Vitis/2022.2/settings64.sh
 set -x
 
 target_name=btle_ll
+
+# arm-linux-gnueabihf-gcc -static -pthread \
+#   -O3 -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard \
+#   -flto -fvisibility=hidden \
+#   -fPIC -fPIE -fstack-protector-strong -fno-common \
+#   -ffunction-sections -fdata-sections \
+#   -Wl,--gc-sections -Wl,-z,relro -Wl,-z,now -Wl,--exclude-libs,ALL -pie \
+#   -o $target_name $target_name.c
+
+# arm-linux-gnueabihf-strip --strip-all $target_name
+
+# arm-linux-gnueabihf-objcopy --redefine-sym old=new --remove-section=.note.gnu.build-id $target_name
+
 arm-linux-gnueabihf-gcc -static -pthread \
   -O3 -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard \
-  -flto -fvisibility=hidden \
-  -fPIC -fPIE -fstack-protector-strong -fno-common \
-  -ffunction-sections -fdata-sections \
-  -Wl,--gc-sections -Wl,-z,relro -Wl,-z,now -Wl,--exclude-libs,ALL -pie \
   -o $target_name $target_name.c
 
-arm-linux-gnueabihf-strip --strip-all $target_name
-
-arm-linux-gnueabihf-objcopy --redefine-sym old=new --remove-section=.note.gnu.build-id $target_name
 # scp $target_name root@10.10.10.10:
 
 # cp $target_name ../../../BTLE/verilog/btle_ll/
