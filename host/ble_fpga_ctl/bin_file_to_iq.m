5dd29cb56b378f21e50a2528af6bdf7d58f97699
Xianjun Jiao <putaoshu@msn.com>
2026-04-12 22:47:16 +0200

    Add flexible data format support for bin_file_to_iq.m

--- host/ble_fpga_ctl/bin_file_to_iq.m
+++ host/ble_fpga_ctl/bin_file_to_iq.m
@@ -1,14 +1,15 @@
 % Xianjun Jiao; putaoshu@msn.com
 
-function iq = bin_file_to_iq(filename_bin)
-% bin file: copmlex, uint8, i/q interleaved
+function iq = bin_file_to_iq(filename_bin, data_format)
+% bin file: copmlex, format (uint8, int16, etc.), i/q interleaved
 fid = fopen(filename_bin);
 if fid == -1
     disp('fopen failed!');
     return;
 end
 
-iq = fread(fid, inf, 'uint8');
+##iq = fread(fid, inf, 'uint8');
+iq = fread(fid, inf, data_format);
 fclose(fid);
 
 iq(1:2:end) = iq(1:2:end) - mean(iq(1:2:end));
