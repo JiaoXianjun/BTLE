265b96dc53e5322ffed5221b577a43bbbfb8eb52
Xianjun Jiao <putaoshu@msn.com>
2026-04-28 21:54:37 +0200

    Let wawter fall show correct x y unit/label

--- host/ble_fpga_ctl/water_fall.m
+++ host/ble_fpga_ctl/water_fall.m
@@ -1,5 +1,5 @@
 % Xianjun Jiao; putaoshu@msn.com
-function a = water_fall(iq, fft_size, num_sample_feed_to_fft, sample_resolution)
+function a = water_fall(iq, fft_size, num_sample_feed_to_fft, sample_resolution, sampling_rate_hz)
 num_col = floor((length(iq)-num_sample_feed_to_fft+1)/sample_resolution);
 a = zeros(fft_size, num_col);
 for i = 1 : num_col
@@ -11,7 +11,23 @@ end
 fft_size_half = fft_size/2;
 a = [a((fft_size_half+1):end,:); a(1:fft_size_half,:)];
 
+x = [1, size(a,2)];
+y = [1, size(a,1)];
+
+if exist('sampling_rate_hz', 'var')
+  time_resolution_us = (sample_resolution*(1/sampling_rate_hz))*1e6;
+  x = [0, size(a,2).*time_resolution_us];
+  y = [-sampling_rate_hz/2, sampling_rate_hz/2];
+end
+
 % pcolor(a);
 figure;
-image(a, 'CDataMapping','scaled');
+image(x, y, a, 'CDataMapping','scaled');
 colorbar;
+
+if exist('sampling_rate_hz', 'var')
+  xlabel('Time(us)');
+  ylabel('Freq(Hz)');
+end
+
+set(gca, 'YDir', 'normal');
